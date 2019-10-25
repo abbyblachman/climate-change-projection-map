81d4a906db039341bb31b1614577cba8ea628b57
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-10-24 20:19:29 -0500

    dropdown

--- code/app.js
+++ code/app.js
@@ -3,6 +3,28 @@ $(document).ready(function() {
 
 // variables
     // City
+var clickedCities = [];
+var cities = [{
+    label: 'City #1',
+    data: [30, 40, 50, 60, 70, 80, 90, 100, 110], 
+    backgroundColor: [
+        'rgba(12, 99, 132, 0.2)'
+    ],
+    borderColor: [
+        'rgba(12, 99, 132, 1)'
+    ],
+    borderWidth: 1
+}, {
+    label: 'City #2',
+    data: [110, 100, 90, 80, 70, 60, 50, 40, 30], 
+    backgroundColor: [
+        'rgba(255, 99, 132, 0.2)'
+    ],
+    borderColor: [
+        'rgba(255, 99, 132, 1)'
+    ],
+    borderWidth: 1
+}]
 
 // functions
 
@@ -18,59 +40,14 @@ $(document).ready(function() {
             // Replace div with table of temperature projections by year that city 
             // Replace div with graph of temperature projections for that city 
 
-    // Clear graph function 
-
-// events
-
-    // Submit dropdown 
-        // Dropdown function 
-
-    // Click station
-        // Click station function 
-
-    // Clear graph 
-
-// init 
-
-    // Sample city (Chicago)
-
+    function chartOne(cityData) {
+    $('.js-chart').html('<canvas id="myChart" width="10rem" height="10rem"></canvas>');
     var ctx = document.getElementById('myChart');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['2020', '2030', '2040', '2050', '2060', '2070'],
-            datasets: [{
-                label: 'City #1',
-                data: [30, 40, 50, 60, 70, 80, 90, 100, 110], 
-                backgroundColor: [
-                    'rgba(12, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(12, 99, 132, 1)'
-                ],
-                borderWidth: 1
-            },
-            {
-                label: 'City #2',
-                data: [110, 100, 90, 80, 70, 60, 50, 40, 30], 
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'/*,
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'*/
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)'/*,
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'*/
-                ],
-                borderWidth: 1
-            }]
+            datasets: cityData
         },
         options: {
             scales: {
@@ -84,4 +61,45 @@ $(document).ready(function() {
         //
         
     });
+}
+
+function init() {
+    $('.js-chart').empty();
+    clickedCities = [];
+}
+
+
+    // Clear graph function 
+
+// events
+
+    // Submit dropdown 
+        // Dropdown function 
+$( ".target" ).change(function() {
+    clickedCities.push(cities[1]);
+    chartOne(clickedCities);
+    });
+    
+$('.js-submit-one').change(function() {
+    clickedCities.push(cities[0]);
+    chartOne(clickedCities);
+})
+
+$('.js-submit-two').change(function() {
+    clickedCities.push(cities[1]);
+    console.log(clickedCities);
+    chartOne(clickedCities);
+})
+
+    // Click station
+        // Click station function 
+
+    // Clear graph 
+$('.js-clear').on('click', function() {
+    init();
+})
+// init 
+init();
+    // Sample city (Chicago)
+
 });
