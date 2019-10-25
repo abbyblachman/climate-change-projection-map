a24aa0628ad7b5a85f2c7fadc2b4dc63b9c0ab49
Abby Blachman <ablachman@Abbys-MacBook-Pro.local>
2019-10-24 19:05:05 -0500

    adding chart

--- code/app.js
+++ code/app.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {  
 //jquery init 
 
 // variables
@@ -32,3 +33,55 @@
 // init 
 
     // Sample city (Chicago)
+
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['2020', '2030', '2040', '2050', '2060', '2070'],
+            datasets: [{
+                label: 'City #1',
+                data: [30, 40, 50, 60, 70, 80, 90, 100, 110], 
+                backgroundColor: [
+                    'rgba(12, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(12, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: 'City #2',
+                data: [110, 100, 90, 80, 70, 60, 50, 40, 30], 
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'/*,
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'*/
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'/*,
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'*/
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+        //
+        
+    });
+});
